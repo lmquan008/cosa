02747a17c76c4be40898f53e137629d799ec85c2
Quan Le Minh <lmquan008@gmail.com>
2016-06-03 16:45:19 +0700

    Update

---
+++ src/impl/com/cosa/mc/impl/ele/AbstractRule.java
@@ -0,0 +1,41 @@
+package com.cosa.mc.impl.ele;
+
+import java.time.LocalDate;
+
+import com.cosa.mc.api.ele.Rule;
+import com.cosa.mc.api.ele.RuleCondition;
+import com.cosa.mc.api.ele.Value;
+
+public abstract class AbstractRule implements Rule {
+
+	@Override
+	public Value getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isDefault() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalDate getStartDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalDate getEndDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RuleCondition getRuleCondition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
